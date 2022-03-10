ab924046fe0e5681d408f32b8624fa3b3d242a5b
Stefan <stefan.blos@gmail.com>
2022-03-10 23:32:32 +0100

    Remove unused initially created file

--- Tests/EffectsLibraryTests/EffectsLibraryTests.swift
+++ Tests/EffectsLibraryTests/EffectsLibraryTests.swift
@@ -6,6 +6,6 @@ final class EffectsLibraryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(EffectsLibrary().text, "Hello, World!")
+//        XCTAssertEqual(EffectsLibrary().text, "Hello, World!")
     }
 }
