b16f63f74f7eeb0928f7fa3ce36e38432450d232
Stefan <stefan.blos@gmail.com>
2022-03-17 13:55:50 +0100

    Added contents and resources for the Snow effect

--- Package.swift
+++ Package.swift
@@ -23,7 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EffectsLibrary",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "EffectsLibraryTests",
             dependencies: ["EffectsLibrary"]),
