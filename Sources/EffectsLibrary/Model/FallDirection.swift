46054631aa304178b4e325b13cbe9b39a12608df
Stefan <stefan.blos@gmail.com>
2022-03-30 15:47:26 +0200

    Documentation for FallDirection added

--- Sources/EffectsLibrary/Model/FallDirection.swift
+++ Sources/EffectsLibrary/Model/FallDirection.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+/// The `FallDirection` enum gives you the option to specify if an effects particles should either fall up or down.
+///
+/// There are 2 possible values here:
+///   - `.upwards`:  Particles will fall from the bottom to the top.
+///   - `.downwards`: Particles will fall from the top to the bottom (default).
 public enum FallDirection {
     case upwards, downwards
 }
